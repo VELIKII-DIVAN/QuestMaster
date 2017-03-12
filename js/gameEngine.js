be6357b9ff974526b4d33825bdca4c18fe81acad
Minaev Dmitriy <dieselbrain@gmail.com>
2017-03-12 20:36:30 +0300

    Skill not worked

--- js/gameEngine.js
+++ js/gameEngine.js
@@ -2,19 +2,42 @@ function QuestMaster(domContainer, gameScript, currentState = null) {
 	this.gameCommands = gameScript.split("$$");
 	this.container = domContainer;
 	this.state = {
-		pointer: 0;		// current command
-	}
+		pointer: 0		// current command
+	};
+
+	this._commandParseRegexp = /\$\$(\w+)\s([\d\D]*)/;
+
 	$.extend(true, this.state, currentState);
 }
 
-QuestMaster.prototype._init = function() {
+this.prototype._personTemplate = {
+	name: "",
+	size: 1,
+	place: ""
+};
+
+this.prototype._placeTemplate = {
+	name: "",
+	menuAlias: []
+}
+
+QuestMaster.prototype._initGame = function() {
+	var scriptLen = this.gameCommands.length;
+	for(var i = 0; i < scriptLen; i += 1) {
+		var block = this.gameCommands[i];
+		if(block === '') continue;
+		this._commandParseRegexp.exec()
+	}
+	if( this.state.pointer == 0 ) {
+		this.state.pointer = i;
+	}
 }
 
 
 QuestMaster.prototype.runGame = function() {
-	this._init();
+	this._initGame();
 	var scriptLen = this.gameCommands.length;
-	for(this.state.pointer; this.state.pointer < scriptLen; this.state.pointer) {
-		
+	for(this.state.pointer; this.state.pointer < scriptLen; this.state.pointer += 1) {
+
 	}
 }
