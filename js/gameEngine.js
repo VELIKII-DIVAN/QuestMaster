f7d82f7e72e04f3a114a8f98c789e534f6286cca
Minaev Dmitriy <dima.garant@gmail.com>
2017-03-13 21:07:26 +0300

    replaced regexp, js not supported unicode

--- js/gameEngine.js
+++ js/gameEngine.js
@@ -5,7 +5,27 @@ function QuestMaster(domContainer, gameScript, currentState = null) {
 		pointer: 0		// current command
 	};
 
-	this._commandParseRegexp = /\$\$(\w+)\s([\d\D]*)/;
+// I try regexp, but don't work with unicode
+	this._parser = {
+		cmd: function(str) {
+			var ret = array('','');
+			for(var i=0; i < str.length; i++) {
+				var char = str[i];
+				if(char == ' ' || char == "\n") {
+					break;
+				}
+			}
+			if(i < 3) {
+				return false;		//bad command
+			}
+			ret[0] = str.substring(2, i);
+			if(i < str.length) {
+				ret[1] = str.substring(i + 1);
+			}
+			return ret;
+		}
+
+	};
 
 	$.extend(true, this.state, currentState);
 }
@@ -26,7 +46,21 @@ QuestMaster.prototype._initGame = function() {
 	for(var i = 0; i < scriptLen; i += 1) {
 		var block = this.gameCommands[i];
 		if(block === '') continue;
-		this._commandParseRegexp.exec()
+		var commandArr = this._regexp["cmd"]();
+		if(commandArr == null || commandArr === false) {
+			// TODO: Maybe should write to some error log
+			continue;
+		}
+		var cmd = commandArr[1];
+		var args = commandArr[2];
+		switch(cmd) {
+			case "comments":
+				continue;
+
+				case "place":
+
+
+		}
 	}
 	if( this.state.pointer == 0 ) {
 		this.state.pointer = i;
